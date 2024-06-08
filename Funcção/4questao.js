2ca90f6abc0bfb3bc6787f290448469afb04d878
Alan Michel <130312363+AlanMichel30@users.noreply.github.com>
2024-06-07 21:05:49 -0300

    Add files via upload

--- Funcção/4questao.js
+++ Funcção/4questao.js
@@ -1,5 +1,5 @@
 function imc(altura, peso){
-   var Imc = peso /Math.pow(altura, 2);
-   console.log(Imc.toFixed(2));
-}
-imc(1.70, 60);  
+    var Imc = peso /Math.pow(altura, 2);
+    console.log(Imc.toFixed(2));
+ }
+ imc(1.70, 60);  
